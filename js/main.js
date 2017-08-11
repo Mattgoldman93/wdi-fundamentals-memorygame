f2ffe481540eff5de6266eb3af9c1a512217d985
Matthew Goldman <matthewgoldman@Matthews-MacBook-Pro.local>
2017-08-11 11:24:09 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-console.log("user flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
+var cardId;
+var checkForMatch = function() {
+    
     if (cardsInPlay[0] === cardsInPlay[1]) {
         alert("You found a match!");
     } else {
         alert("Sorry, try again.");
     }
 }
+var flipCard = function(cardId) {
+
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
+}
+}
+flipCard(0);
+flipCard(2);
+
+ 
